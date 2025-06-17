ab764695555a5c29d82063c8b3dc519a0e713de2
Johanna <103958711+johanna-skylight@users.noreply.github.com>
2025-06-17 14:06:32 -0400

    Lint checks pipeline (#3)
    * Added data access folder
    
    * Fixed formatting
    
    * Added lint and unit test checks
    
    * Added npm ci and prettier check step
    
    * Reorganizes jobs in workflow
    
    * Removed pre build step

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,28 @@
-'use client';
+"use client";
 
 import classNames from "classnames";
 import { Button } from "@trussworks/react-uswds";
 
 const Home: React.FC = () => {
   return (
-      <div
-        className={classNames(
-          "page-container",
-          "page-container--full-height"
-        )}
+    <div
+      className={classNames("page-container", "page-container--full-height")}
+    >
+      <h1>Welcome!</h1>
+      <p className={classNames("margin-bottom-4")}>
+        This is a shell NextJS application already integrated with dependencies
+        commonly used in federal goverment apps, like USWDS, eslint, prettier,
+        jest, playwright.
+      </p>
+      <Button
+        onClick={() => {
+          alert("Hello there!");
+        }}
       >
-        <h1>Welcome!</h1>
-        <p className={classNames("margin-bottom-4")}>This is a shell NextJS application already integrated with dependencies commonly used in federal goverment apps, like USWDS, eslint, prettier, jest, playwright.</p>
-        <Button onClick={()=>{
-          alert("Hello there!")
-        }}>Click me!</Button>
-      </div>
+        Click me!
+      </Button>
+    </div>
   );
 };
-
-
 
 export default Home;
